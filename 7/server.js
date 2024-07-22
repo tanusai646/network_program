c77cf3632e4d28a74184be5a0f09be0a184fc7af
yuki <imayuu1010@gmail.com>
2024-07-22 14:09:33 +0900

    変更

--- 7/server.js
+++ 7/server.js
@@ -2,11 +2,11 @@ let ws = require('ws')                      // JSONのWS（WebSocket）パッケ
 var server = new ws.Server({port:8080});    // WebSocketサーバの生成 ～ ポート番号8080を利用
 server.on('connection', ws => {             // WSサーバ機能の実行 ～ クライアント接続時に対応
     ws.on('message', message => {           // クライアントからのデータ受信（messageに格納）時に実行
-    console.log(message);                   // 1) サーバの標準出力にmessageの内容表示
-    ws.send(message);                       // 2) 送信クライアントのみにmessageの内容返信
+    console.log(message.toString("utf8"));                   // 1) サーバの標準出力にmessageの内容表示
+    ws.send(message.toString("utf8"));                       // 2) 送信クライアントのみにmessageの内容返信
     server.clients.forEach(client => {      // サーバ接続の全クライアントに対する処理の規定
         if (client !== ws)                  // 送信クライアントを除外する条件設定
-        client.send(message);               // 3) 接続クライアントへのmessageの内容送信
+        client.send(message.toString("utf8"));               // 3) 接続クライアントへのmessageの内容送信
         // ※ 表示で文字化けする場合 (message) の部分を全て (message.toString("utf8") ) に変更
         });
     });
