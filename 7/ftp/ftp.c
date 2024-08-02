377323748308e5070e90a35b2e3396497b215d8f
yuki <imayuu1010@gmail.com>
2024-08-02 11:17:26 +0900

    川原研に接続完了

---
+++ 7/ftp/ftp.c
@@ -0,0 +1,47 @@
+/*222C1021 今村優希*/
+/*ftp.c*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main(){
+    int sockfd;
+    struct sockaddr_in address;
+    char buf[80] = "\0";
+
+    //INETドメイン、ストリームソケットを利用
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //サーバー情報を設定
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("131.206.37.50");
+    address.sin_port = htons(50000);
+
+    //クライアントからの接続要求とサーバ情報を確認
+    int res = connect(sockfd, (struct sockaddr *)&address, sizeof(address));
+
+    if(res == -1){
+        perror("error\n");
+        exit(1);
+    }
+    printf("\n * server IP: %s, port: %d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+    strcpy(buf, "client will connect to server");
+    write(sockfd, buf, strlen(buf));
+    
+    // サーバからのデータ受信
+    memset(buf, '\0', sizeof(buf)); // buf[]読み込み前に初期化
+    read(sockfd, buf, sizeof(buf));
+    printf("\n * message from server : %s \n", buf);
+
+    //ソケットの除去
+    close(sockfd);
+}
