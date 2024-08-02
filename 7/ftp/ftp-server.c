e4f0355b586deae477746a4677ee7a3d9dbf50b5
yuki <imayuu1010@gmail.com>
2024-08-02 13:40:21 +0900

    server作成の準備

---
+++ 7/ftp/ftp-server.c
@@ -0,0 +1,65 @@
+/*222C1021 今村優希*/
+/*server3.c*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main(){
+    int sockfd_s, fd, pid, pwait;
+    struct sockaddr_in address_s;
+    char buf[80] = "\0";
+
+    //INETドメイン、ストリームソケットを利用
+    sockfd_s = socket(AF_INET, SOCK_STREAM, 0);
+
+    //サーバソケット=(IP: 10.0.2.15, ポート:5000)に設定
+    address_s.sin_family = AF_INET;
+    address_s.sin_addr.s_addr = inet_addr("10.0.2.15");
+    address_s.sin_port = htons(5000);
+
+    bind(sockfd_s, (struct sockaddr *)&address_s, sizeof(address_s));
+
+    //要求受付の準備
+    listen(sockfd_s, 5);
+    printf("sertver waits\n");
+
+    //接続要求の許可と情報確認
+    struct sockaddr_in address_c;
+    unsigned int length_c = sizeof(address_c);
+
+    int sockfd_c = accept(sockfd_s, (struct sockaddr *)& address_c, &length_c);
+
+    /*子プロセスでファイルの送受信を行う*/
+    pid = fork();
+    while(1){
+        if(pid == 0){
+            printf("\n * request from client IP: %s, port %d\n", inet_ntoa(address_c.sin_addr), ntohs(address_c.sin_port));
+            memset(buf, '\0', sizeof(buf));
+            read(sockfd_c, buf, sizeof(buf));
+            printf("\n* message from client: %s\n", buf);
+
+            strcpy(buf, "From Server via socket");
+            write(sockfd_c, buf, strlen(buf));
+
+            close(sockfd_c);
+            close(sockfd_s);
+        } else{
+            pwait = waitpid(-1, &status, WNOHANG);
+            printf("sarver wait\n");
+        }
+    }
+
+    //送受信操作
+
+
+    //ソケットの除去
+
+ }
