9850d07a3fd1fe1a7216b0e34b0dbfe47a3502d8
yuki <imayuu1010@gmail.com>
2024-08-04 21:03:31 +0900

    レポート用

--- report/last-report/code/ftp-client.c
+++ report/last-report/code/ftp-client.c
@@ -1,5 +1,5 @@
 /*222C1021 今村優希*/
-/*ftp.c*/
+/*ftp-client.c*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,11 +22,11 @@ int main(){
 
     //サーバー情報を設定
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr("131.206.37.50");
-    address.sin_port = htons(50000);
+    address.sin_addr.s_addr = inet_addr("10.0.2.15");
+    address.sin_port = htons(5000);
 
     //クライアントからの接続要求とサーバ情報を確認
-    //川原研のサーバーに接続
+    //サーバーに接続
     int res = connect(sockfd, (struct sockaddr *)&address, sizeof(address));
 
     if(res == -1){
@@ -34,7 +34,6 @@ int main(){
         exit(1);
     }
     printf("\n * server IP: %s, port: %d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-
     
     //サーバーに学生番号を送信
     printf("Your ID:");
@@ -43,8 +42,8 @@ int main(){
 
     printf("read start\n");
 
-    /*222C1021-copy.txtのオープン*/
-    fd = open("222C1021-copy.txt", O_WRONLY);
+    /*222C1021-test.txtのオープン*/
+    fd = open("222C1021-test.txt", O_WRONLY);
     if(fd == -1){
         fprintf(stderr, "can't open the file\n");
         exit(1);
@@ -54,15 +53,15 @@ int main(){
     //入力がexit_sendになるまで読み込み、.txtに書き込む
     while(1){
         memset(buf, '\0', sizeof(buf)); // buf[]読み込み前に初期化
-        int point = read(sockfd, buf, sizeof(buf));
+        read(sockfd, buf, sizeof(buf));
         write(fd, buf, strlen(buf));
-        if(strcmp(buf, end) == 0){
+        if(strstr(buf, end) != NULL){
             printf("read finish\n");
             break;
         }
     } 
 
-    close(fd);      //222C1021-copy.txtのクローズ
+    close(fd);      //222C1021-test.txtのクローズ
     close(sockfd);  //ソケットの除去
 
 }
---
+++ report/last-report/code/ftp-server.c
@@ -0,0 +1,81 @@
+/*222C1021 今村優希*/
+/*ftp-server.c*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main(){
+    int sockfd_s, fd, pid, pwait;
+    struct sockaddr_in address_s;
+    char buf[80] = "\0";
+    char txt[] = ".txt";
+    char end[10] = "exit_send";
+
+    //INETドメイン、ストリームソケットを利用
+    sockfd_s = socket(AF_INET, SOCK_STREAM, 0);
+
+    //サーバソケット=(IP: 10.0.2.15, ポート:5000)に設定
+    address_s.sin_family = AF_INET;
+    address_s.sin_addr.s_addr = inet_addr("10.0.2.15");
+    address_s.sin_port = htons(5000);
+
+    bind(sockfd_s, (struct sockaddr *)&address_s, sizeof(address_s));
+
+    //要求受付の準備
+    listen(sockfd_s, 5);
+    printf("sertver waits\n");
+
+    //接続要求の許可と情報確認
+    struct sockaddr_in address_c;
+    unsigned int length_c = sizeof(address_c);
+
+    /*子プロセスでファイルの送受信を行う*/
+    pid = fork();
+    while(1){
+        int sockfd_c = accept(sockfd_s, (struct sockaddr *)& address_c, &length_c);
+        pid = fork();
+        if(pid == 0){
+            printf("\n * request from client IP: %s, port %d\n", inet_ntoa(address_c.sin_addr), ntohs(address_c.sin_port));
+            //学生番号の取得
+            memset(buf, '\0', sizeof(buf));
+            read(sockfd_c, buf, sizeof(buf));
+            
+            //学生番号と.txtを結合
+            strcat(buf, txt);
+            printf("%s\n", buf);    //debug
+            fd = open(buf, O_RDONLY);
+            if(fd == -1){
+                printf("file can't the open\n");
+                exit(1);
+            }
+
+            printf("start write\n");
+            while(1){
+                memset(buf, '\0', sizeof(buf)); // buf[]読み込み前に初期化
+                read(fd, buf, sizeof(buf));     //ファイルの内容をbufに格納
+                write(sockfd_c, buf, strlen(buf));//送信
+                if(strstr(buf, end) != NULL){
+                    printf("finished write\n");
+                    break;
+                }
+            } 
+            close(sockfd_c);
+            close(sockfd_s);
+
+            exit(1);
+        } else{
+            int status;
+            pwait = waitpid(-1, &status, WNOHANG);
+            printf("sarver wait\n");
+        }
+    }
+ }
